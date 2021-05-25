97bf889e90438f5ccc1d6d27417122364fc56550
Severn Everett <severn.everett@gmail.com>
2021-05-25 21:13:22 +0200

    Added configuration for device name, server address, and topic

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -1,22 +1,37 @@
 use std::error::Error;
+use uuid::Uuid;
 
 use crate::lib::common::{IllegalArgumentError, RuntimeError, RuntimeMode};
+use config::ConfigError;
 
 pub struct RunnerConfig {
+    pub device_name: String,
+    pub server_address: String,
+    pub topic: String,
     pub runtime_mode: RuntimeMode,
     pub check_interval: u64,
 }
 
+// Configuration key names
+const DEVICE_NAME_KEY: &str = "device_name";
 const RUNTIME_MODE_KEY: &str = "runtime_mode";
+const CHECK_INTERVAL_KEY: &str = "check_interval";
+const SERVER_ADDRESS_KEY: &str = "server_address";
+const TOPIC_KEY: &str = "topic";
+// Configuration values
+const DEFAULT_SERVER_ADDRESS: &str = "tcp://localhost:1883";
+const DEFAULT_TOPIC: &str = "Device_Status";
 const SINGLE_RUNTIME_MODE: &str = "Single";
 const CONTINUOUS_RUNTIME_MODE: &str = "Continuous";
-const CHECK_INTERVAL_KEY: &str = "check_interval";
 const DEFAULT_CHECK_INTERVAL: u64 = 1;
 const MINIMUM_CHECK_INTERVAL: u64 = DEFAULT_CHECK_INTERVAL;
 const MAXIMUM_CHECK_INTERVAL: u64 = 240;
 
 pub fn load_config(config_path: Option<&String>) -> Result<RunnerConfig, Box<dyn Error>> {
     let mut runner_config = RunnerConfig {
+        device_name: Uuid::new_v4().to_string(),
+        server_address: String::from(DEFAULT_SERVER_ADDRESS),
+        topic: String::from(DEFAULT_TOPIC),
         runtime_mode: RuntimeMode::Single,
         check_interval: DEFAULT_CHECK_INTERVAL,
     };
@@ -32,11 +47,28 @@ pub fn load_config(config_path: Option<&String>) -> Result<RunnerConfig, Box<dyn
             return Err(error);
         }
     };
+    // Device name
+    match settings.get_str(DEVICE_NAME_KEY) {
+        Ok(device_name) => runner_config.device_name = device_name,
+        Err(_) => {}
+    };
+    // Server address
+    match settings.get_str(SERVER_ADDRESS_KEY) {
+        Ok(server_address) => runner_config.server_address = server_address,
+        Err(_) => {}
+    };
+    // Topic
+    match settings.get_str(TOPIC_KEY) {
+        Ok(topic) => runner_config.topic = topic,
+        Err(_) => {}
+    };
+    // Runtime mode
     match settings.get_str(RUNTIME_MODE_KEY) {
         Ok(mode) => {
             match mode.as_str() {
                 CONTINUOUS_RUNTIME_MODE => {
                     runner_config.runtime_mode = RuntimeMode::Continuous;
+                    // Check interval
                     match settings.get(CHECK_INTERVAL_KEY) {
                         Ok(check_interval) => {
                             if check_interval >= MINIMUM_CHECK_INTERVAL && check_interval <= MAXIMUM_CHECK_INTERVAL {
@@ -52,10 +84,15 @@ pub fn load_config(config_path: Option<&String>) -> Result<RunnerConfig, Box<dyn
                             }
                         }
                         Err(e) => {
-                            let error = Box::new(
-                                IllegalArgumentError::new(e.to_string().as_str())
-                            );
-                            return Err(error);
+                            match e {
+                                ConfigError::NotFound(_) => {},
+                                _ => {
+                                    let error = Box::new(
+                                        IllegalArgumentError::new(e.to_string().as_str())
+                                    );
+                                    return Err(error);
+                                }
+                            }
                         }
                     }
                 }
@@ -77,31 +114,64 @@ pub fn load_config(config_path: Option<&String>) -> Result<RunnerConfig, Box<dyn
 #[cfg(test)]
 mod tests {
     use pretty_assertions::assert_eq;
+    use pretty_assertions::assert_ne;
 
     use crate::lib::common::{IllegalArgumentError, RuntimeMode};
-    use crate::lib::config::{DEFAULT_CHECK_INTERVAL, load_config};
+    use crate::lib::config::{DEFAULT_CHECK_INTERVAL, load_config, DEFAULT_SERVER_ADDRESS, DEFAULT_TOPIC};
 
     #[test]
     fn load_default_config() {
         let result = load_config(None).unwrap();
+        assert_ne!("", result.device_name);
+        assert_eq!(DEFAULT_SERVER_ADDRESS, result.server_address);
+        assert_eq!(DEFAULT_TOPIC, result.topic);
         assert_eq!(RuntimeMode::Single, result.runtime_mode);
         assert_eq!(DEFAULT_CHECK_INTERVAL, result.check_interval);
     }
 
     #[test]
-    fn load_standalone_config() {
+    fn load_bare_single_config() {
         let result = load_config(
-            Some(&String::from("resources/test/good/ideal_standalone.yaml"))
+            Some(&String::from("resources/test/good/bare_single.yaml"))
         ).unwrap();
+        assert_ne!("", result.device_name);
+        assert_eq!(DEFAULT_SERVER_ADDRESS, result.server_address);
+        assert_eq!(DEFAULT_TOPIC, result.topic);
+        assert_eq!(RuntimeMode::Single, result.runtime_mode);
+    }
+
+    #[test]
+    fn load_bare_continuous_config() {
+        let result = load_config(
+            Some(&String::from("resources/test/good/bare_continuous.yaml"))
+        ).unwrap();
+        assert_ne!("", result.device_name);
+        assert_eq!(DEFAULT_SERVER_ADDRESS, result.server_address);
+        assert_eq!(DEFAULT_TOPIC, result.topic);
+        assert_eq!(RuntimeMode::Continuous, result.runtime_mode);
+        assert_eq!(DEFAULT_CHECK_INTERVAL, result.check_interval);
+    }
+
+    #[test]
+    fn load_full_single_config() {
+        let result = load_config(
+            Some(&String::from("resources/test/good/full_single.yaml"))
+        ).unwrap();
+        assert_eq!("Test Device Name", result.device_name);
+        assert_eq!("tcp://test.server.address:1883", result.server_address);
+        assert_eq!("Test Topic", result.topic);
         assert_eq!(RuntimeMode::Single, result.runtime_mode);
         assert_eq!(DEFAULT_CHECK_INTERVAL, result.check_interval);
     }
 
     #[test]
-    fn load_continuous_config() {
+    fn load_full_continuous_config() {
         let result = load_config(
-            Some(&String::from("resources/test/good/ideal_continuous.yaml"))
+            Some(&String::from("resources/test/good/full_continuous.yaml"))
         ).unwrap();
+        assert_eq!("Test Device Name", result.device_name);
+        assert_eq!("tcp://test.server.address:1883", result.server_address);
+        assert_eq!("Test Topic", result.topic);
         assert_eq!(RuntimeMode::Continuous, result.runtime_mode);
         assert_eq!(5, result.check_interval);
     }
